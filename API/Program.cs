0e5143773e2dac0c5b78a8bc1c50094397d7dc60
soumikctg <soumikctg616@gmail.com>
2024-02-12 13:09:54 +0600

    clientInitital

--- API/Program.cs
+++ API/Program.cs
@@ -13,10 +13,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder => builder.AllowAnyHeader().WithOrigins("https://localhost:4200"));
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
