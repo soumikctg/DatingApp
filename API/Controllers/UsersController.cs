d8644d4a9aa863abef28d755f774ccaedc0ef7f6
soumikctg <soumikctg616@gmail.com>
2024-02-12 20:06:24 +0600

    validation

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,9 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseAPIController
     {
         private readonly DataContext _context;
 
